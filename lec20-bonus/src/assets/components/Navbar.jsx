9d1b1919b7282860e312b682c7dcf745b5877a80
tamanna-2703 <tamannasachdeva27@gmail.com>
2025-11-23 15:25:56 +0530

    done

---
+++ lec20-bonus/src/assets/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = (props) => {
+
+    function changeTheme(){
+        props.setTheme('Dark')
+    }
+  return (
+    <div>
+      <button onClick={changeTheme}> Change Theme</button>
+    </div>
+  )
+}
+
+export default Navbar
